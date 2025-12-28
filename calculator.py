8ef5dc3ad62e92e21637d452bc2c14290fc2a1ef
Lorenzo Astolfi <lorenzo.astolfi2208@gmail.com>
2025-12-28 10:48:33 +0100

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -25,13 +25,13 @@ while verify:
     print(logo)
 
     F_Num = float(input("What's the first number: "))
-    oper = operation[input("+\n-\n*\n/\nPick an operation: ")]
+    oper = input("+\n-\n*\n/\nPick an operation: ")
     S_Num = float(input("What's the second number: "))
-    Result = oper(n1 = F_Num, n2 = S_Num)
+    Result = operation[oper](n1 = F_Num, n2 = S_Num)
     print(f"{F_Num} {oper} {S_Num} = {Result}")
     while input(f"Type 'y' to continue calculating with {Result}, or type 'n' to start a new calculation: ").lower() == "y":
-        oper = operation[input("+\n-\n*\n/\nPick an operation: ")]
+        oper = input("+\n-\n*\n/\nPick an operation: ")
         F_Num = float(input("What's the successive number: "))
         print(f"{Result} {oper} {F_Num} = {oper(n1 = Result, n2 = F_Num)}")
-        Result = oper(n1=Result, n2=F_Num)
+        Result = operation[oper](n1 = F_Num, n2 = S_Num)
     os.system('cls||clear')
